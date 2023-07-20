0f75971f442a68b018322a3c4fa08b855ee05d27
Святослав <svatoslavsuvorov956@gmail.com>
2023-07-20 12:57:55 +0300

    дз 1 номер 4

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-'пытаюсь подкличть гх'
+'''
+
+
+
+
+
+
+'''
+
+
+
+# def counter (s):
+#     for i in set(s):
+#         count = 0
+#         for j in s:
+#             if i == j:
+#                 count += 1
+#         print(i, count)
+#
+# counter('abcdabbcadad')
+
+
+# def counter (s):
+#     syms = {}
+#     for i in s:
+#         syms[i] = syms.get(i, 0) + 1
+#     for i, count in syms.items():
+#         print(i, count)
+#
+# counter('shcfjshfjkewkyfb')
+
+
+# h\w:
+def is_palindrome(string):
+    return string == string[::-1]
+
+print(is_palindrome('арозаупаланалапуазора'))
+
+
+
+
